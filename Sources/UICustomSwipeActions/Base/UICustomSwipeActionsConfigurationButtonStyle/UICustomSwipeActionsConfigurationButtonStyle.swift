f6f287ab597bfc19b348c1237e1640b993d18aef
sebjvidal <seb.vidal@btinternet.com>
2024-07-03 18:45:00 +0100

    Rename `UICustomSwipeActionsConfigurationButtonStyle`

---
+++ Sources/UICustomSwipeActions/Base/UICustomSwipeActionsConfigurationButtonStyle/UICustomSwipeActionsConfigurationButtonStyle.swift
@@ -0,0 +1,13 @@
+//
+//  UICustomSwipeActionsConfigurationButtonStyle.swift
+//
+//
+//  Created by Seb Vidal on 18/11/2023.
+//
+
+import Foundation
+
+public enum UICustomSwipeActionsConfigurationButtonStyle {
+    case `default`
+    case circular
+}
